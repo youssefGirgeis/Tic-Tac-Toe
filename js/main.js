57c0ef42e64c75c7974efb7248c6c1d6ae5d172c
Youssef Girgeis <youssefgirgeis@gmail.com>
2021-10-29 16:05:54 -0400

    show main page and hide board whem page loads

--- js/main.js
+++ js/main.js
@@ -1,18 +1,21 @@
-var mainHtml = '';
+// let mainHtml = "";
 
-function mainPage(){
-    
-     mainHtml = '<div class="screen screen-start" id="start">' +
-        '<header>' +
-        '<h1>' + 'Tic Tac Toe' + '</h1>' +
-        '<a href="#" class="button">'+ 'Start game' + '</a>' +
-        '</header>' +
-        '</div>'
-    
-    
-    
-}
+// function mainPage() {
+//   mainHtml =
+//     '<div class="screen screen-start" id="start">' +
+//     "<header>" +
+//     "<h1>" +
+//     "Tic Tac Toe" +
+//     "</h1>" +
+//     '<a href="#" class="button">' +
+//     "Start game" +
+//     "</a>" +
+//     "</header>" +
+//     "</div>";
+// }
+//document.getElementById("start").innerHTML = mainHtml;
+// window.onload = mainPage;
 
-
-document.getElementById('start').innerHTML = mainHtml;
-//window.onload = mainPage;
+const showMainPage = () => {
+  document.getElementById("board").style.display = "none";
+};
